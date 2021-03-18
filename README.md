3d597709d9b4497099ca21bbfe029c674f95d03a
Yatish Bhardwaj <yatishbhardwaj1@gmail.com>
2021-03-19 04:33:11 +0530

    Cleaned Vanilla Arch Script

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Scripts
 
-This repository contains all of my installation scripts.
+This repository contains all of my installation scripts. Most of them are self explanatory and are made for Arch-based or Debian-based distros. 
 
-Most of them are self explanatory and are made for Arch-based or Debian-based distros.
+Please use with caution and read the script beforehand.
 
-I am in no way responsible if you break your installation or OS! Please use with caution and read the script beforehand.
+I am in no way responsible if you break your system! 
--- vanilla-arch/software.sh
+++ vanilla-arch/software.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "################### WELCOME TO SOFTWARE INSTALLATION SCRIPT #########################"
+echo "################### EXECUTING SOFTWARE INSTALLATION SCRIPT #########################"
 
 # Install Pacman Stuff
 sudo pacman -S --noconfirm ranger shotwell
@@ -8,14 +8,18 @@ sudo pacman -S --noconfirm ranger shotwell
 # Install AUR Stuff
 yay -S --noconfirm google-chrome visual-studio-code-bin xdman
 
+sudo pacman -S flatpak
+
+echo "################### OPTIONALS ##############################"
 echo "Do you want to install Virt-manager? (y/n)"
 read -r install_choice
+
 if ["$install_choice" = "y"]; then
      sudo pacman -S virt-manager qemu vde2 libguestfs openbsd-netcat dnsmasq ebtables libvirt
      sudo systemctl enable libvirtd.service
      sudo systemctl start libvirtd.service
 else
-     echo "Virt-manager not installing"
+     echo "Not installing Virt-manager"
 fi
 
 echo "SETUP COMPLETE!"
